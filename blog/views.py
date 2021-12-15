7e786d49ed61a919cdfeb1ee40cc32dffb9c809a
Pejman Ghafouri <pejmanghafouri666@gmail.com>
2021-12-14 21:05:33 -0500

    django-blog-078

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,5 @@
 from django.views.generic import ListView, DetailView
 from account.models import User
-from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
 from account.mixins import AuthorAccessMixin
 # from django.http import HttpResponse, JsonResponse, Http404
